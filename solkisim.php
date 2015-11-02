e329ae340822e2394a20739cb806fb5e9154f5a8
emirhanuzun01 <emrhnuzn@gmail.com>
2015-11-02 10:27:48 +0200

    Create solkisim.php

---
+++ solkisim.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+
+
+<div class="list-group">
+  <a href="index.php" class="list-group-item active">
+    YBS
+  </a>
+  <a href="index.php?menuid=1" class="list-group-item">Hakkında</a>
+  <a href="index.php?menuid=2" class="list-group-item">Akademik Personel</a>
+  <a href="index.php?menuid=3" class="list-group-item">Lisans</a>
+  <a href="index.php?menuid=4" class="list-group-item">İletişim</a>
+</div>
+
+
+

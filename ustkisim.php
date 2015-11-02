c15b5bbe2d7a60806c81c06a2c4b4b73b92a09ee
emirhanuzun01 <emrhnuzn@gmail.com>
2015-11-02 10:28:24 +0200

    Create ustkisim.php

---
+++ ustkisim.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+
+<div class="row"> 
+
+    <div class="col-lg-2"> <img src="resimler/btu_logo.png" alt=""/> </div>
+    <div class="col-lg-1"><a href="index.php">ÖBS</a></div>
+    <div class="col-lg-1"><a href="index.php">ÖBS</a></div>
+    <div class="col-lg-1"><a href="index.php">ÖBS</a></div>
+    <div class="col-lg-1"><a href="index.php">ÖBS</a></div>
+    <div class="col-lg-1"><a href="index.php">ÖBS</a></div>
+    <div class="col-lg-1"><a href="index.php">ÖBS</a></div>
+    <div class="col-lg-1"><a href="index.php">ÖBS</a></div>
+    <div class="col-lg-1"><a href="index.php">ÖBS</a></div>
+    
+    <div class="col-lg-2"> <img src="resimler/ataturk.png" alt=""/> </div>
+    
+    
+    
+</div>
